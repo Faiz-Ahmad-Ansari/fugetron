d2ce2b8e91bd0eed4e730025df9640212a0bce20
faiz <ansarifaiz32@gmail.com>
2021-05-29 02:35:24 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project for fugetron
         </a>
       </header>
     </div>
