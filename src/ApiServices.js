3e7b4c1500e8966b5d93788717efc76587ad8fa9
faiz <ansarifaiz32@gmail.com>
2021-05-30 16:48:46 +0530

    minor refinement in ApiServices

--- src/ApiServices.js
+++ src/ApiServices.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
+const common = 'execute-api.us-east-1.amazonaws.com/v1';
+
 const ApiServices = {
 
     /////////// FETCH RECORD LIST ///////////
     recordList(){
-        return axios.get('https://j5ej5u32gg.execute-api.us-east-1.amazonaws.com/v1/fetch')
+        return axios.get(`https://j5ej5u32gg.${common}/fetch`)
     },
     /////////// ADD NEW RECORD ///////////
     addRecord(queryString){
-        return axios.get(`https://c0ri699qs5.execute-api.us-east-1.amazonaws.com/v1/add?${queryString}`)
+        return axios.get(`https://c0ri699qs5.${common}/add?${queryString}`)
     },
     /////////// EDIT RECORD ///////////
     editRecord(queryString){
-        return axios.get(`https://o1wm686yz2.execute-api.us-east-1.amazonaws.com/v1/edit?${queryString}`)
+        return axios.get(`https://o1wm686yz2.${common}/edit?${queryString}`)
     },
     /////////// DELETE RECORD ///////////
     deleteRecord(queryString){
-        return axios.get(`https://k6j938wg66.execute-api.us-east-1.amazonaws.com/v1/delete?param1=${queryString}`)
+        return axios.get(`https://k6j938wg66.${common}/delete?param1=${queryString}`)
     },
 
 }
